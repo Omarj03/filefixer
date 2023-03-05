ff20153160196aba4b50eddbb833a0946ba7a5cb
omarJ <ojundi03@gmail.com>
2023-03-05 19:08:53 +0000

    - revamped get_dir() to add options to quickly select downloads, documents and desktop - now runs get_dir() on startup rather than defaulting to user downloads directory - fixed error where 'files' list is attempted to be indexed while its length is 0

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
-from os import path
 import control as c
 
 
 # Menu
-def cli(user_dl_path):
+def cli(working_dir):
     while True:
         print(f'\n' * 20)
-        print(f'''FileFixer Build 04/03/2023
-Current Directory: {user_dl_path}
+        print(f'''FileFixer Build 05/03/2023
+Current Directory: {working_dir}
 --------------------------
 Please select an option below:
     1. Sort one filetype
-    2. sort all files over threshold
-    3. sort all files
+    2. Sort all files over threshold
+    3. Sort all files
     4. Change directory
     5. Exit
 --> ''', end='')
@@ -26,7 +25,7 @@ Please select an option below:
                         if '.' in filetype:
                             print("Invalid input. Please enter filetype, ommitting the '.'.")
                         else:
-                            c.sort_files(user_dl_path, filetype)
+                            c.sort_files(working_dir, filetype)
                             break
 
                 case '2':
@@ -39,11 +38,11 @@ Please select an option below:
                         except ValueError as e:
                             print(f"Error: {e},Please enter a number (integer) greater than 0.")
                             c.to_continue()
-                    c.sort_files(user_dl_path, "all", threshold)
+                    c.sort_files(working_dir, "all", threshold)
                 case '3':
-                    c.sort_files(user_dl_path)
+                    c.sort_files(working_dir)
                 case '4':
-                    user_dl_path = c.get_dir()
+                    working_dir = c.get_dir()
                 case '5':
                     break
                 case _:
@@ -53,12 +52,10 @@ Please select an option below:
             print(f"\nError: {e},  Please try again or exit application.")
             c.to_continue()
 
-
+# Main startup
 def main():
-    user_home_path = path.expanduser('~')
-    user_dl_path = path.join(user_home_path, "Downloads")
-
-    cli(user_dl_path)
+    working_dir = c.get_dir()
+    cli(working_dir)
 
 
 if __name__ == "__main__":
